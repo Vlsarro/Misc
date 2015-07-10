8d16d51cef9e5a8517c55ce8f2497c50485a5688
Vlsarro <drekmaro@gmail.com>
2015-07-11 01:32:39 +0300

    Added postresql with postgis extension

--- Misc/settings.py
+++ Misc/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+# skey - is a module, that contains secret key and database password
+from skey import Seckey, dbpass 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = Seckey
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -76,8 +79,11 @@ WSGI_APPLICATION = 'Misc.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'gmap',
+        'USER': 'postgres',
+        'PASSWORD': dbpass,
+        'HOST': 'localhost',
     }
 }
 
