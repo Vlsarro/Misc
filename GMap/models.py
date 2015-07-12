906142d88b782852f32735df63e0a816a251bc19
Vlsarro <drekmaro@gmail.com>
2015-07-12 18:59:59 +0300

    added main model

--- GMap/models.py
+++ GMap/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.gis.db import models as gis_models
+from django.contrib.gis import geos
 
 # Create your models here.
+
+class kindergarten(models.Model):
+	name = models.CharField(max_length = 200)
+	address = models.CharField(max_length = 100)
+	city = models.CharField(max_length = 50)
+	location = gis_models.PointField(u"longitude/latitude",
+									 geography = True, blank = True)
+
+	gis = gis_models.GeoManager()
+	objects = models.Manager()
+
+
+def __unicode__(self):
+	return self.name
