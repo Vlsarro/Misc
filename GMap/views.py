acab35c2fac512f0b5d8ae2c59361a15a6213a7f
Vlsarro <drekmaro@gmail.com>
2015-07-13 23:49:22 +0300

    started writing code for views

--- GMap/views.py
+++ GMap/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from urllib2 import URLError
 
-# Create your views here.
+from django.shortcuts import render
+from django.contrib.gis import geos
+from django.contrib.gis import measure
+from django.template import RequestContext
+
+from geopy.geocoders.google import Google
+from geopy.geocoders.google import GQueryError
+
+from gmap import forms
+from gmap import models
+
+
+def geocode_address(address):
+	address = address.encode('utf-8')
+	geocoder = Google()
+	try:
+		_, latlon = geocoder.geocode(address)
+	except (URLError, GQueryError, ValueError):
+		return None
+	else:
+		return latlon
