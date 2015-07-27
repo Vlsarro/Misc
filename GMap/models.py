c2689d4d4c5fe785a749350d8fd9b81ae77831cd
Vlsarro <drekmaro@gmail.com>
2015-07-27 16:57:38 +0300

    got it all together, ready for testing

--- GMap/models.py
+++ GMap/models.py
@@ -1,19 +1,34 @@
+from urllib2 import URLError
+
 from django.db import models
 from django.contrib.gis.db import models as gis_models
 from django.contrib.gis import geos
+from geopy import geocoders
 
-# Create your models here.
 
 class kindergarten(models.Model):
-	name = models.CharField(max_length = 200)
-	address = models.CharField(max_length = 100)
-	city = models.CharField(max_length = 50)
-	location = gis_models.PointField(u"longitude/latitude",
-									 geography = True, blank = True)
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    location = gis_models.PointField(u"longitude/latitude",
+                                     geography=True, null =True, blank=True)
 
-	gis = gis_models.GeoManager()
-	objects = models.Manager()
+    gis = gis_models.GeoManager()
+    objects = models.Manager()
 
+    def __unicode__(self):
+        return self.name
 
-def __unicode__(self):
-	return self.name
+    def save(self, **kwargs):
+        if not self.location:
+            address = u'%s %s' % (self.city, self.address)
+            address = address.encode('utf-8')
+            geocoder = geocoders.GoogleV3()
+            try:
+                _, latlon = geocoder.geocode(address)
+            except (URLError, ValueError):
+                pass
+            else:
+                point = "POINT(%s %s)" % (latlon[1], latlon[0])
+                self.location = geos.fromstr(point)
+            super(kindergarten, self).save()
--- GMap/views.py
+++ GMap/views.py
@@ -1,49 +1,53 @@
 from urllib2 import URLError
 
+from django.contrib.gis.geos import Point
 from django.shortcuts import render_to_response
-from django.contrib.gis import geos
+# from django.contrib.gis import geos
 from django.contrib.gis import measure
 from django.template import RequestContext
 
 from geopy import geocoders
 
-from GMap import forms
-from GMap import models
+from GMap import forms, models
 
 
 def geocode_address(address):
 
-	address = address.encode('utf-8')
-	geocoder = geocoders.Google()
-	try:
-		_, latlon = geocoder.geocode(address)
-	except (URLError, GQueryError, ValueError):
-		return None
-	else:
-		return latlon
+    address = address.encode('utf-8')
+    geocoder = geocoders.GoogleV3()
+    try:
+        _, latlon = geocoder.geocode(address)
+    except (URLError, ValueError):
+        return None
+    else:
+        return latlon
 
 
 def get_objects(longitude, latitude):
 
-	current_point = geos.fromstr("POINT(%s %s)" % (longitude, latitude))
-	distance_from_point = {'km': 10}
-	objects = models.kindergarten.gis.filter(location_distance_lte=(current_point,
-											measure.D(**distance_from_point)))
-	objects = objects.distance(current_point).order_by('distance')
-	return objects.distance(current_point)
+    # current_point = geos.fromstr("POINT(%s %s)" % (longitude, latitude))
+    current_point = Point(longitude, latitude)
+    distance_from_point = {'km': 30}
+    objects = models.kindergarten.gis.filter(location__distance_lte=(current_point,
+                                            measure.D(**distance_from_point)))
+    objects = objects.distance(current_point).order_by('distance')
+    return objects.distance(current_point)
 
 
 def home(request):
-	form = forms.AddressForm()
-	kindergartens = []
-	if request.POST:
-		form = forms.AddressForm(request.POST)
-		if form.is_valid():
-			address = form.cleaned_data('address')
-			location = geocode_address(address)
-			if location:
-				latitude, longitude = location
-				kindergartens = get_objects(longitude, latitude)
+    form = forms.AddressForm()
+    kindergartens = []
+    if request.POST:
+        form = forms.AddressForm(request.POST)
+        if form.is_valid():
+            address = form.cleaned_data['address']
+            location = geocode_address(address)
+            if location:
+                latitude, longitude = location
+                kindergartens = get_objects(longitude, latitude)
 
-	return render_to_response('home.html', {'form': form, 'kindergartens': kindergartens},
-				context_instance=RequestContext(request))
+    return render_to_response(
+        'home.html',
+        {'form': form, 'kindergartens': kindergartens},
+        context_instance=RequestContext(request))
+
